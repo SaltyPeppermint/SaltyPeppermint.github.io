daa3be513efd3c5f52dc27c50222e34fba68b2dd
Nicole Heinimann <heinimann@tu-berlin.de>
2025-12-30 11:27:03 +0100

    formatted

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,14 @@
-import type { APIContext } from 'astro';
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import type { APIContext } from "astro";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context: APIContext) {
-  const blog = await getCollection('blog', ({ data }) => !data.draft);
+  const blog = await getCollection("blog", ({ data }) => !data.draft);
 
   return rss({
     title: "Nicole Thalia Heinimann's Blog",
-    description: "Blog posts from Nicole Thalia Heinimann - PhD Student at TU Berlin",
+    description:
+      "Blog posts from Nicole Thalia Heinimann - PhD Student at TU Berlin",
     site: context.site!,
     items: blog
       .sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf())
